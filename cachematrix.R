ff4164b0834fcd25f74a60a85babc15cdc0ddf57
Lionel Barnabas Low <info@flashmech.net>
2015-01-24 19:30:53 +0800

    Add more comments to explain the code, and changed cc to ci to better reflect that it's "cache inverse"

--- cachematrix.R
+++ cachematrix.R
@@ -3,38 +3,43 @@
 ## cacheSolve will solve the inverse of the wrapped matrix
 ## produced by makeCacheMatrix
 
-## Returns a list with 4 functions to interact with 'x'
-## function - set -
+## Returns a list with 4 functions to interact with 'x'. These 4 functions
+## reside in their own environment, not on the global environment
+## function - set - sets the value of matrix 'x'
 ## function - get - returns the value of matrix 'x'
 ## function - setinverse - sets the inverse of matrix 'x'
 ## function - getinverse - returns null if the inverse of matrix 'x'
 ## does not exist, or the inverse of matrix 'x'
 makeCacheMatrix <- function(x = matrix()) {
-	# variable cc will store the cache of the inverse matrix
-	cc <- NULL
+	# variable ci (cached inverse) will store the cache of the inverse matrix
+	ci <- NULL
 	set <- function (y) {
 		x <<- y
-		cc <<- NULL
+		ci <<- NULL
 	}
 	get <- function () x
-	setinverse <- function (inverse) cc <<- inverse
-	getinverse <- function () cc
+	setinverse <- function (inverse) ci <<- inverse
+	getinverse <- function () ci
 	list(set = set, get = get,
 			 setinverse = setinverse,
 			 getinverse = getinverse)
 }
 
 ## Return a matrix that is the inverse of 'x'
+## This function assumes that the matrix in 'x' is always invertible.
 ## If 'x' has cached data, it will return the cached matrix
 ## Otherwise, it will solve the inverse of 'x'
 cacheSolve <- function(x, ...) {
-	cc <- x$getinverse()
-	if (!is.null(cc)) {
+	# variable ci (cached inverse) will attempt to retrieve
+	# the cache of the inverse matrix
+	ci <- x$getinverse()
+	# this check runs when ci contains data
+	if (!is.null(ci)) {
 		message("getting cached data")
-		return(cc)
+		return(ci)
 	}
 	data <- x$get()
-	cc <- solve(data, ...)
-	x$setinverse(cc)
-	cc
+	ci <- solve(data, ...)
+	x$setinverse(ci)
+	ci
 }
